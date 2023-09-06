986fecd7f8e905abefd294017473b50843dd3634
mohanapriya2174 <132121558+mohanapriya2174@users.noreply.github.com>
2023-09-06 19:13:39 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-console.log("hello world");
+/*console.log("hello world");
 console.log(typeof"mona");
 console.log(typeof 21);
-console.log(typeof true);
+console.log(typeof true);*/
+// const myVar="mathematic";
+// console.log(myVar.lastIndexOf("t"));
+// console.log(myVar.slice(5,8));
+// console.log(myVar.toUpperCase());
+// console.log(myVar.includes("ma"));
+// console.log(myVar.split(""));
+const myNum =23;
+console.log(myNum);
+const myFloat = 23.0;
+const myString="23";
+console.log(myNum===myString);
+console.log(Number(true));
+
+
