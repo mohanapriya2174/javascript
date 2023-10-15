c877f12ed0ace0a701582efd3a7c1bdfdf6d4dee
mohanapriya2174 <132121558+mohanapriya2174@users.noreply.github.com>
2023-10-15 14:54:36 +0530

    changes

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// let js='hi';
+// // if (js==='hi') alert('hello');
+// console.log( typeof true);
+// let jacascriptIsFun="true";
+// console.log(typeof true);
+// console.log(typeof jacascriptIsFun);
+// console.log(typeof 23);
+
+// jacascriptIsFun="YES";
+// console.log(typeof jacascriptIsFun);
+
+// const age = 10;
+// const isOldEnough=age>=19;
+// if(isOldEnough){
+//     console.log("Sara can start driving👍");
+// }
+// else{
+//     const yearsLeft=18-age;
+//     console.log(`sara had to wait for ${yearsLeft} to start draiving`);
+// }
+
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("one"));
+// console.log(Boolean(""));
+
+// let age=0;
+// if(age) console.log("age is defined");
+// else console.log("UNDEFINED");
+
+
+// const favPerson=Number(prompt("who is your fav person?"));
+// console.log(favPerson);
+// if(favPerson===23){
+//     console.log("cool one day he will become your boyFriend");
+// }
+
+// const hasDrvingLicene=true;
+// const hasGoodVission=false;
+// console.log(hasDrvingLicene&&hasGoodVission);
+// console.log(hasDrvingLicene||hasGoodVission);
+
+// const age=19;
+// age>=18? console.log("i can drink"):console.log("i can't drink");
+// console.log(`wine..! ${age>=18? "i can drink!":"i can't drink"}`);
+
---
+++ js/script2.js
@@ -0,0 +1,22 @@
+
+// function gramCal(eachWeights){
+//     return eachWeights * 25;
+// }
+
+
+
+// function foodPrefered(idli,vada){
+//     const idliWeight=gramCal(idli);
+//     const vadaWeight=gramCal(vada);
+//     // console.log(idli,vada);
+//     const food=`i would like to have ${idliWeight} grams ofidlies and ${vadaWeight} grams of vadas.👍😊💕`;
+//     return food;
+// }
+// console.log(foodPrefered(2,1));
+// const breakFast=foodPrefered(3,1);
+// console.log(breakFast);
+// console.log(foodPrefered(3,1));
+
+// const calAge=birthYear=>2023-birthYear;
+// // const age=calAge(2004);
+// console.log(`i'm ${calAge(2004)} now!😎😁😀`);
