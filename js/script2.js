491b02c4754e98bb6ec27e5c0dc072efa09f45ca
mohanapriya2174 <mohanapriya274@gmail.com>
2023-10-31 19:47:29 -0700

    changes

--- js/script2.js
+++ js/script2.js
@@ -37,26 +37,74 @@
 // };
 // const wishTo=prompt("what would you like to know about mona");
 // console.log(mona[wishTo]);
-const mona = {
-    firstName:'mona',
-    lastName:'priya',
-    birthYear: 2004,
-    profession:"student",
-    frinds:['sanga','saran'],
-    driverLicence:true,
-    calAge: function (){
-        // console.log(this);
-        return 2023-this.birthYear;
-    },
-    getSummary:function(){
-        return(`${this.firstName} is a ${this.profession}, and she has ${this.driverLicence?'a':'no'} driving licence`);
-    }
-};
-console.log(mona.calAge());
+// const mona = {
+//     firstName:'mona',
+//     lastName:'priya',
+//     birthYear: 2004,
+//     profession:"student",
+//     frinds:['sanga','saran'],
+//     driverLicence:true,
+//     calAge: function (){
+//         // console.log(this);
+//         return 2023-this.birthYear;
+//     },
+//     getSummary:function(){
+//         return(`${this.firstName} is a ${this.profession}, and she has ${this.driverLicence?'a':'no'} driving licence`);
+//     }
+// };
+// console.log(mona.calAge());
 
-// console.log(mona.calAge());
-// console.log(mona.calAge());
-// console.log(mona.calAge());
+// // console.log(mona.calAge());
+// // console.log(mona.calAge());
+// // console.log(mona.calAge());
  
-console.log(mona.getSummary());
-console.log(`${mona.firstName} is a ${mona.calAge()} year old girl, and she has ${mona.driverLicence?'a':'no'} driving licence`);
+// console.log(mona.getSummary());
+// console.log(`${mona.firstName} is a ${mona.calAge()} year old girl, and she has ${mona.driverLicence?'a':'no'} driving licence`);
+//--------------------------------------------
+
+// const Mark={
+//     fullName:'Mark Miller',
+//     mass:78,
+//     height:1.69,
+//     calcBMI:function(){
+//        return this.mass/this.height**2;
+//     }
+// }
+// const John={
+//     fullName:"John Smith",
+//     mass:92,
+//     height:1.95,
+//     calcBMI:function(){
+//         return this.mass/this.height**2;
+//      }
+   
+// }
+// console.log(Mark.calcBMI());
+// console.log(John.calcBMI());
+// if(John.calcBMI>Mark.calcBMI ){
+//     console.log(`${John.fullName}'s BMI(${John.calcBMI()}) is greater than ${Mark.fullName}'s BMI(${Mark.calcBMI()})`);
+// }
+// else{
+//     console.log(`${Mark.fullName}'s BMI(${Mark.calcBMI()}) is greater than ${John.fullName}'s BMI(${John.calcBMI()})`);
+// }
+//--------------------------------------------------------------
+// const monaArrey=[
+//     'mona',
+//     'priya',
+//     2023-2004,
+//     ['sanga','aarthi']
+// ];
+// const newNew=[];
+// for(i=0;i<monaArrey.length;i++){
+//     console.log(monaArrey[i]);
+//     newNew[i]=monaArrey[i];
+// //     newNew[i]=typeof monaArrey[i];
+// }
+// console.log(newNew);
+
+let dice=Math.trunc(Math.random() * 6)+1;
+
+while(dice!==6){
+    console.log (`you rolled a ${dice}`);
+    dice=Math.trunc(Math.random() * 6)+1;
+}
